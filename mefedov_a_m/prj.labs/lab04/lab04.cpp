024e33f158067efdac0d74474359ffdb1cd5223f
Антоний Мефедов <antonmefedov@icloud.com>
2022-03-14 18:47:34 +0300

    update readme

--- mefedov_a_m/prj.labs/lab04/lab04.cpp
+++ mefedov_a_m/prj.labs/lab04/lab04.cpp
@@ -4,28 +4,33 @@
 
 
 int main() {
+        
+    std::string video = "../../../data/videoMoney.mp4";
+    cv::VideoCapture videocap1 = cv::VideoCapture(video);
 
-        cv::VideoCapture video("C:/Imagine/mefedov_a_m/data/videoMoney.mp4");
+    if (!videocap1.isOpened())
+        std::cout << "Error openning video" << std::endl;
 
-        if (!video.isOpened())
-            std::cout << "Error openning video" << std::endl;
-        else {
-            int fps = video.get(5);
-        }
+    cv::Mat frame[3];
+    uint16_t framesNumber = videocap1.get(cv::CAP_PROP_FRAME_COUNT);
 
-        /*if (!video.isOpened())
-        {
-            return -1;
-        }
+    for (int i = 0; i < framesNumber; ++i) {
+        if (i == (framesNumber / 5) * 2)
+            videocap1 >> frame[0];
+        if (i == (framesNumber / 5) * 3)
+            videocap1 >> frame[1];
+        if (i == (framesNumber / 5) * 4)
+            videocap1 >> frame[2];
 
-        cv::Mat frame;
-        for (int i = 0; i < video.get(cv::CAP_PROP_FRAME_COUNT) / 5; i++) {
-            if (i == 2 || 3 || 4) {
-                std::cout << "find" << "\n";
-            }
-        }
-        */
+        videocap1.grab();
+        std::cout << videocap1.get(cv::CAP_PROP_FRAME_COUNT) << " ";
+    }
 
+    cv::imshow("frame1", frame[0]);
+    cv::imshow("frame2", frame[1]);
+    cv::imshow("frame3", frame[2]);
+   
+    cv::waitKey(0);
 
-        return 0;
+     return 0;
 }
